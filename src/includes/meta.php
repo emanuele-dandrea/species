d4df006c2960cf5e698f4a0b9f1482fbd081cd8f
Emanuele D'Andrea <emanueledandrea@proton.me>
2026-04-03 21:56:09 +0200

    src: add meta.php to includes

---
+++ src/includes/meta.php
@@ -0,0 +1,6 @@
+        <link rel="icon" type="image/png" href="/public/favicon/favicon-96x96.png?v=20260403" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/public/favicon/favicon.svg?v=20260403" />
+        <link rel="shortcut icon" href="/public/favicon/favicon.ico?v=20260403" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/public/favicon/apple-touch-icon.png?v=20260403" />
+        <meta name="apple-mobile-web-app-title" content="Species" />
+        <link rel="manifest" href="/public/favicon/site.webmanifest?v=20260403" />
