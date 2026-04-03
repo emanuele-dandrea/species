bb0333bfe9d6c264218b05adcc258ac00f85f8d8
Emanuele D'Andrea <emanueledandrea@proton.me>
2026-04-03 22:02:12 +0200

    update footer and header

--- src/includes/header.php
+++ src/includes/header.php
@@ -1,11 +1,40 @@
+<?php
+$menu = [
+    'home' => 'Home',
+    'login' => 'Enter <i class="bi bi-door-open-fill"></i>'
+];
+
+$currentDir = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+?>
+
 <header>
     <div class="width flex-wrapper padding">
-        <h1>Species</h1>
+        <div id="logo" class="flex-wrapper no-select">
+            <img src="/public/images/logo.svg" alt="Logo" draggable="false">
+            <h1>Species</h1>
+        </div>
         <nav>
             <ul class="gap flex-wrapper">
-                <li><a href="/auth/registration/">Registration</a></li>
-                <li><a href="/auth/login/">Login</a></li>
+                <?php foreach ($menu as $dir => $label):
+                    $isActive = ($dir === 'home' && $currentDir === '') || ($currentDir === $dir) ? 'active' : '';
+                    $href = ($dir === 'home') ? '/' : '/' . $dir . '/';
+                ?>
+                    <li class="<?= $isActive ?>">
+                        <a href="<?= $href ?>"><?= $label ?></a>
+                    </li>
+                <?php endforeach; ?>
             </ul>
         </nav>
     </div>
 </header>
+
+<style>
+div.flex-wrapper{
+    gap: 1em;
+}
+#logo {
+    img {
+        width: 3em;
+    }
+}
+</style>
