faf9a4e14aa450f5b72e8558ecb21b162814bc4b
Emanuele D'Andrea <emanueledandrea@proton.me>
2026-04-04 21:42:53 +0200

    add includes/search.php

---
+++ src/includes/search.php
@@ -0,0 +1,20 @@
+<button class="search-bar" dir="rtl">Search &nbsp;<i class="bi bi-search"></i></button>
+
+<style>
+button.search-bar {
+    cursor: pointer;
+    color: var(--bg-color);
+    background-color: var(--fg-color);
+    padding: 0em 3em;
+    border-radius: var(--border-radius);
+    font-size: 1em;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 1em;
+    border: 0.1em solid gray;
+    transition: opacity ease-in-out 0.3s;
+}
+button.search-bar:hover {
+    opacity: 0.9;
+}
+</style>
