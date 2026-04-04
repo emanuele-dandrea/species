97fe19ffde0eab6249fd4e4caba6d23d4fd773a3
Emanuele D'Andrea <emanueledandrea@proton.me>
2026-04-04 21:44:54 +0200

    add search button to homepage

--- src/index.php
+++ src/index.php
@@ -11,7 +11,15 @@
 
 <body>
     <?php include './includes/header.php'; ?>
-
+    <section class="width padding flex-wrapper" style="align-items: start; justify-content: start; gap: 5em;">
+        <div>
+            <h2>Our encyclopedia for more<br>equal <i>knowledge</i></h2>
+            <? include './includes/search.php'?>
+        </div>
+        <div>
+            <img src="./public/images/Cats-cradle.svg" alt="useless decoration" draggable="false" class="no-select" style="width: 15em;">
+        </div>
+    </section>
     <main class="width padding flex-wrapper">
         <section>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit suscipit delectus quidem quos mollitia assumenda cumque, aut fugit, expedita commodi eveniet natus porro ut possimus culpa nulla libero molestiae iure.</p>
@@ -25,3 +33,14 @@
 </body>
 
 </html>
+<style>
+    section div img {
+        filter: brightness(0) saturate(100%) invert(100%) sepia(0%) saturate(1849%) hue-rotate(84deg) brightness(112%) contrast(62%);
+    }
+
+    @media (prefers-color-scheme: light) {
+        section div img {
+            filter: brightness(0) saturate(100%) invert(14%) sepia(7%) saturate(0%) hue-rotate(144deg) brightness(90%) contrast(96%);
+        }
+    }
+</style>
